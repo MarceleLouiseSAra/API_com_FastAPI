c6b939cee461e2fa972e8bdac560c59e816d7bfe
MarceleLouiseSAra <louise20marcele03@gmail.com>
2024-11-30 17:22:24 -0300

    feat: migrations quebrada

--- migrations/versions/77a72fde2956_create_users_table.py
+++ migrations/versions/77a72fde2956_create_users_table.py
@@ -1,8 +1,8 @@
 """create users table
 
-Revision ID: affc0764dd86
+Revision ID: 77a72fde2956
 Revises: 
-Create Date: 2024-11-28 12:21:53.874576
+Create Date: 2024-11-28 12:34:19.789667
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'affc0764dd86'
+revision: str = '77a72fde2956'
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
